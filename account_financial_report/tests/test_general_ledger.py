e99381c3e36b0e39d223d18e541d842331d3229a
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-01-28 02:53:56 +0200

    [FIX] pylint

--- account_financial_report/tests/test_general_ledger.py
+++ account_financial_report/tests/test_general_ledger.py
@@ -505,9 +505,12 @@ class TestGeneralLedgerReport(common.TransactionCase):
         partner_3.write({'is_company': False})
 
         expected_list = [partner_2.id, partner_3.id, partner_4.id]
-        context = {'active_ids': [
-            partner_1.id, partner_2.id, partner_3.id, partner_4.id],
-                   'active_model': 'res.partner'}
+        context = {
+            'active_ids': [
+                partner_1.id, partner_2.id, partner_3.id, partner_4.id
+                ],
+            'active_model': 'res.partner'
+            }
 
         wizard = self.env["general.ledger.report.wizard"].with_context(context)
         self.assertEqual(wizard._default_partners(), expected_list)
